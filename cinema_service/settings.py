ddcdf131af838603bff351bab711f822020d20ad
Andrii <yurkivandriy02@gmail.com>
2025-04-28 17:51:24 +0300

    Fix flake8 issues

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
 }
